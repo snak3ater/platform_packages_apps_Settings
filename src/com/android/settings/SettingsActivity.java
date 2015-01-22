4cfee55f509b7d2bc33e6732c2eafa2a67423441
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-22 13:26:47 +0000

    QS: add newly added tiles
    * Expanded desktop
    * Screen off
    Change-Id: I64956333bd22d26db6ce72a6149b496d4542bfec
    
    Conflicts:
    	res/values/strings.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -69,6 +69,7 @@ import com.android.settings.accessibility.AccessibilitySettings;
 import com.android.settings.accessibility.CaptionPropertiesFragment;
 import com.android.settings.accounts.AccountSettings;
 import com.android.settings.accounts.AccountSyncSettings;
+import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
@@ -307,7 +308,8 @@ public class SettingsActivity extends Activity
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
             BlacklistSettings.class.getName(),         
-            DisplayRotation.class.getName()
+            DisplayRotation.class.getName(),          
+            ExpandedDesktopPreferenceFragment.class.getName()
     };
 
 
