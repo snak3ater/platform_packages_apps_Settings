6fc4c61cf6f246e3f3da9fba39eb64857069b215
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-01 07:23:50 +0000

    QS: add Sync tile (2/2)
    Change-Id: Ieb3eddd45be2a8c6c4bf19b5931c40998794324d
    
    Conflicts:
    	res/values/strings.xml

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -132,6 +132,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_power";
                 stringId = R.string.qs_screen_off_tile;
                 break;
+            case QSConstants.TILE_SYNC:
+                resourceName = "ic_qs_sync_on";
+                stringId = R.string.qs_sync_tile;
+                break;
             default:
                 return null;
         }
